821587924d10174e691300b4e4fdf79ca3db776f
Gabriel Santos <gab.tec97@gmail.com>
2023-04-24 17:46:13 -0300

    login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('login', views.login, name="login"),
+    path('login', auth_views.LoginView.as_view(
+        template_name="accounts/login.html"
+    ), name="login"),
     path('logout', auth_views.LogoutView.as_view(), name="logout"),
     path('signup', views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,11 +10,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("core:home")
+            return redirect("home")
     else:
         form = SignUpForm()
     return render(request, "accounts/signup.html", {"form": form})
-
-
-def login(request):
-    pass
