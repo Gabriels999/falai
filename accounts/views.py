ff2c757e3483c9284ceed96819e18d9130c4af71
Gabriel Santos <gab.tec97@gmail.com>
2023-04-24 16:36:57 -0300

    bota ordem na casa. cria a app accounts e a tela de signup

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
+from accounts.forms import SignUpForm
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("core:home")
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {"form": form})
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
--- falaai/settings.py
+++ falaai/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "channels",
-    "core"
+    "core",
+    "accounts",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
